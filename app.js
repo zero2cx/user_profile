9d58aee56cabb675cac9e999145edf99c46750da
David Schenck <zero2cx@gmail.com>
2017-08-03 03:58:23 +0000

    move app routes to new file: routes/user.js

--- app.js
+++ app.js
@@ -5,13 +5,15 @@
 var filename = __filename.split("/").pop();
 var express = require('express');
 var app = express();
-// var router = express.Router({ mergeParams: true }); // var router = express.Router();
+// var router = express.Router({ mergeParams: true });
+// var router = express.Router();
 var mongoose = require('mongoose');
 var methodOverride = require("method-override");
 var bodyParser = require('body-parser');
 var passport = require("passport");
 var LocalStrategy = require("passport-local");
 var User = require("./models/user");
+var userRoutes = require("./routes/user");
 var seedDatabase = require("./seeds");
 var databaseUrl = process.env.DATABASE_URL || "mongodb://localhost/user_profile";
 
@@ -35,6 +37,10 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+
+/**
+ * set some variables scoped for all routes
+ */
 app.use(function(req, res, next) {
   res.locals.currentUser = req.user;
   next();
@@ -52,110 +58,7 @@ app.use(function(req, res, next) {
 });
 
 
-/**
- * ROUTE: GET /
- *  display landing page
- */
-app.get('/', function(req, res) {
-  res.render('landing');
-});
-
-
-/**
- * ROUTE: GET /index
- *  display all users
- */
-app.get('/index', function(req, res) {
-  User.find({}, function(err, users) {
-    if (err) {
-      console.error('1 ** error: [%s] %s', filename, err);
-      res.redirect('/');
-    }
-    res.render('index', {
-      users: users
-    });
-  });
-});
-
-
-/**
- * ROUTE: GET /new
- *  display new user form
- */
-app.get('/new', function(req, res) {
-  res.render('new');
-});
-
-
-/**
- * ROUTE: POST /new
- *  create the current user
- */
-app.post('/new', function(req, res) {
-  var newUser = new User({
-    username: req.body.username,
-    first_name: req.body.first_name,
-    last_name: req.body.last_name,
-    avatar_url: req.body.avatar_url,
-    email_address: req.body.email_address,
-    join_date: req.body.join_date,
-    biography: req.body.biography
-  });
-  User.register(newUser, req.body.password, function(err, user) {
-    if (err) {
-      console.error('2 ** error: [%s] %s', filename, err);
-      return res.redirect('new');
-    }
-    passport.authenticate('local')(req, res, function() {
-      console.log(' ++ info: [%s] create user "%s"', filename, user.username);
-      res.render('show', {
-        user: user
-      });
-    });
-  });
-});
-
-
-/**
- * ROUTE: POST /login
- *  login the current user
- */
-app.post('/login', passport.authenticate('local', {
-  successRedirect: '/index',
-  failureRedirect: '/login'
-}), function(req, res) {
-  //empty callback
-});
-
-
-
-/**
- * ROUTE: GET /logout
- *  log out the user
- */
-app.get('/logout', function(req, res) {
-  req.logout();
-  res.redirect('index');
-});
-
-
-/**
- * ROUTE: GET /:id
- *  display a user profile
- */
-app.get('/:id', function(req, res) {
-  User.findById(req.params.id, function(err, user) {
-    if (err) {
-      console.error('3 ** error: [%s] %s', filename, err);
-      res.redirect('/');
-    }
-    else {
-      res.render('show', {
-        user: user
-      });
-    }
-  });
-});
+app.use(userRoutes);
 
 /**
  *  LISTEN: start the app server
---
+++ routes/user.js
@@ -0,0 +1,115 @@
+/**
+ * file: routes/user.js
+ */
+
+var filename = __filename.split("/").pop();
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+var User = require('../models/user');
+
+/**
+ * ROUTE: GET /
+ *  display landing page
+ */
+router.get('/', function(req, res) {
+  res.render('landing');
+});
+
+
+/**
+ * ROUTE: GET /index
+ *  display all users
+ */
+router.get('/index', function(req, res) {
+  User.find({}, function(err, users) {
+    if (err) {
+      console.error('1 ** error: [%s] %s', filename, err);
+      res.redirect('/');
+    }
+    res.render('index', {
+      users: users
+    });
+  });
+});
+
+
+/**
+ * ROUTE: GET /new
+ *  display new user form
+ */
+router.get('/new', function(req, res) {
+  res.render('new');
+});
+
+
+/**
+ * ROUTE: POST /new
+ *  create the current user
+ */
+router.post('/new', function(req, res) {
+  var newUser = new User({
+    username: req.body.username,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    avatar_url: req.body.avatar_url,
+    email_address: req.body.email_address,
+    join_date: req.body.join_date,
+    biography: req.body.biography
+  });
+  User.register(newUser, req.body.password, function(err, user) {
+    if (err) {
+      console.error('2 ** error: [%s] %s', filename, err);
+      return res.redirect('new');
+    }
+    passport.authenticate('local')(req, res, function() {
+      console.log(' ++ info: [%s] create user "%s"', filename, user.username);
+      res.render('show', {
+        user: user
+      });
+    });
+  });
+});
+
+
+/**
+ * ROUTE: POST /login
+ *  login the current user
+ */
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/index',
+  failureRedirect: '/login'
+}), function(req, res) {
+  //empty callback
+});
+
+
+/**
+ * ROUTE: GET /logout
+ *  log out the user
+ */
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('index');
+});
+
+
+/**
+ * ROUTE: GET /:id
+ *  display a user profile
+ */
+router.get('/:id', function(req, res) {
+  User.findById(req.params.id, function(err, user) {
+    if (err) {
+      console.error('3 ** error: [%s] %s', filename, err);
+      res.redirect('/');
+    }
+    else {
+      res.render('show', {
+        user: user
+      });
+    }
+  });
+});
+
+module.exports = router;
