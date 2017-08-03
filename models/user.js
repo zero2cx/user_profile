d1ca0659c35cd9f63c38ca46e7073a28d6b0d249
David Schenck <zero2cx@gmail.com>
2017-08-03 20:02:17 +0000

    change default-avatar image-url

--- models/user.js
+++ models/user.js
@@ -12,7 +12,9 @@ var UserSchema = new mongoose.Schema({
   username: String,
   avatar_url: {
     type: String,
-    default: 'https://images.duckduckgo.com/iu/?u=http%3A%2F%2F2.bp.blogspot.com%2F-Jkpn4Ghz1Os%2FTkxlMiUN6WI%2FAAAAAAAAANk%2F7e4iMQcDsTg%2Fs1600%2Ffake-facebook-profile-picture-funny-batman-pic.jpg&f=1'
+    /* image courtesy of computerclipart.com; licensed free for educational usage */
+    default: 'http://www.computerclipart.com/computer_clipart_images/silhouette_of_a_cat_playing_with_a_butterfly_0071-1002-1223-4658_SMU.jpg'
+    // default: 'https://images.duckduckgo.com/iu/?u=http%3A%2F%2F2.bp.blogspot.com%2F-Jkpn4Ghz1Os%2FTkxlMiUN6WI%2FAAAAAAAAANk%2F7e4iMQcDsTg%2Fs1600%2Ffake-facebook-profile-picture-funny-batman-pic.jpg&f=1'
   },
   join_date: {
     type: String,
